7abd6de7d48e6df82a8bc9d1789b6f33c87fee8c
Chiazam U <d_ubaniukoma@yahoo.com>
2025-02-22 22:04:21 -0600

    Updated various files

--- AutomationHW_Lesson2_Targetcase.py
+++ AutomationHW_Lesson2_Targetcase.py
@@ -18,16 +18,10 @@ driver.get('https://www.google.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('bandicoot')
+search.send_keys('Car')
 
 # wait for 4 sec
 sleep(4)
 
 # click search button
 driver.find_element(By.NAME, 'btnK').click()
-
-# verify search results
-assert 'bandicoot'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
-print('Test Passed')
-
-driver.quit()
